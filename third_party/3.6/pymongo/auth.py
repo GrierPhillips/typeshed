275e968026c25c59e8fbbf6b0ec0fb432cad3c52
Grier Phillips <GrierPhillips@gmail.com>
2017-11-22 17:01:06 -0700

    Started typeshed for pymongo using Python 3.6 syntax

---
+++ third_party/3.6/pymongo/auth.py
@@ -0,0 +1,43 @@
+from collections import namedtuple
+from typing import Any, Dict, FrozenSet, NamedTuple
+
+from pymongo import pool
+
+
+MECHANISMS: FrozenSet = frozenset(
+    ['GSSAPI', 'MONGODB-CR', 'MONGODB-X509', 'PLAIN', 'SCRAM-SHA-1',
+     'DEFAULT'])
+
+
+MongoCredential: NamedTuple = namedtuple(
+    'MongoCredential',
+    ['mechanism', 'source', 'username', 'password', 'mechanism_properties'])
+
+GSSAPIProperties: NamedTuple = namedtuple('GSSAPIProperties',
+                                          ['service_name',
+                                           'canonicalize_host_name',
+                                           'service_realm'])
+
+def _build_credentials_tuple(mech: str, source: str, user: str, passwd: str,
+                             extra: Dict[str, Any]) -> MongoCredential: ...
+
+def _parse_scram_response(response: bytes) -> Dict[bytes, int]: ...
+def _authenticate_scram_sha1(credentials: MongoCredential,
+                             sock_info: pool.SocketInfo) -> None: ...
+def _password_digest(username: str, password: str) -> str: ...
+def _auth_key(nonce: str, username: str, password: str) -> str: ...
+def _authenticate_gssapi(credentials: MongoCredential,
+                         sock_info: pool.SocketInfo) -> None: ...
+def _authenticate_plain(credentials: MongoCredential,
+                        sock_info: pool.SocketInfo) -> None: ...
+def _authenticate_cram_md5(credentials: MongoCredential,
+                           sock_info: pool.SocketInfo) -> None: ...
+def _authenticate_x509(credentials: MongoCredential,
+                       sock_info: pool.SocketInfo) -> None: ...
+def _authenticate_mongo_cr(credentials: MongoCredential,
+                           sock_info: pool.SocketInfo) -> None: ...
+def _authenticate_default(credentials: MongoCredential,
+                          sock_info: pool.SocketInfo) -> None: ...
+def authenticate(credentials: MongoCredential, sock_info: pool.SocketInfo)\
+    -> None: ...
+def logout(source: str, sock_info: pool.SocketInfo) -> None: ...
