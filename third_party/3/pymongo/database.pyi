84c22e8fdfeb435ad8ad3741e1ce08f8b4b9b9ca
Grier Phillips <GrierPhillips@gmail.com>
2017-11-30 16:20:38 -0700

    Add Sequence

--- third_party/3/pymongo/database.pyi
+++ third_party/3/pymongo/database.pyi
@@ -1,4 +1,4 @@
-from typing import Any, Callable, Dict, List, Mapping, Optional, Union
+from typing import Any, Callable, Dict, List, Mapping, Optional, Sequence, Union
 
 from bson import CodecOptions, DBRef
 from pymongo import MongoClient, common
